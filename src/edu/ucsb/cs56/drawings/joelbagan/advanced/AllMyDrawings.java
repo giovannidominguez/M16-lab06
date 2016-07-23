134fbe1277c3e721b7b4a71ad600c59c61b976eb
joelbagan <joelbagan@yahoo.com>
2016-07-22 21:42:40 -0700

    Created GameBoy and GameBoyWithDisplay, changed the methods in AllMyDrawings to draw GameBoys instead of that other stuff :P

---
+++ src/edu/ucsb/cs56/drawings/joelbagan/advanced/AllMyDrawings.java
@@ -0,0 +1,150 @@
+package edu.ucsb.cs56.drawings.joelbagan.advanced;
+
+import edu.ucsb.cs56.drawings.utilities.ShapeTransforms;
+
+import java.awt.*;
+
+/**
+ * A class with static methods for drawing various pictures
+ *
+ * @author Joel Bagan
+ * @version for UCSB CS56, W16
+ */
+
+public class AllMyDrawings {
+    /**
+     * Draw a picture with a few GameBoys
+     */
+
+    public static void drawPicture1(Graphics2D g2) {
+
+        GameBoy gb1 = new GameBoy(10, 50, 50, 75);
+        g2.setColor(Color.BLUE);
+        g2.draw(gb1);
+
+        // Make a black house that's half the size,
+        // and moved over 150 pixels in x direction
+
+        Shape gb2 = ShapeTransforms.scaledCopyOfLL(gb1, 0.5, 0.5);
+        gb2 = ShapeTransforms.translatedCopyOf(gb2, 150, 0);
+        g2.setColor(Color.BLACK);
+        g2.draw(gb2);
+
+        // Here's a house that's 4x as big (2x the original)
+        // and moved over 150 more pixels to right.
+        gb2 = ShapeTransforms.scaledCopyOfLL(gb2, 4, 4);
+        gb2 = ShapeTransforms.translatedCopyOf(gb2, 150, 50);
+
+        // We'll draw this with a thicker stroke
+        Stroke thick = new BasicStroke(4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
+
+        // for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
+        // #002FA7 is "International Klein Blue" according to Wikipedia
+        // In HTML we use #, but in Java (and C/C++) its 0x
+
+        Stroke orig = g2.getStroke();
+        g2.setStroke(thick);
+        g2.setColor(new Color(0x002FA7));
+        g2.draw(gb2);
+
+        // Draw 2 GameBoys with display
+
+        GameBoyWithDisplay gd1 = new GameBoyWithDisplay(100, 130, 100, 200);
+        GameBoyWithDisplay gd2 = new GameBoyWithDisplay(210, 200, 100, 200);
+
+        g2.setColor(Color.cyan);
+        g2.draw(gd1);
+        g2.setColor(new Color(0xFF25BF));
+        g2.draw(gd2);
+
+        g2.setStroke(orig);
+        g2.setColor(Color.BLACK);
+        g2.drawString("An artful arrangement of GameBoys by Joel Bagan", 20, 20);
+    }
+
+
+    /**
+     * Draw a picture of some GameBoys and GameBoys with displays
+     */
+    public static void drawPicture2(Graphics2D g2) {
+
+        // Draw some coffee cups.
+
+        GameBoy top = new GameBoy(320, 10, 100, 100);
+        GameBoy right = new GameBoy(600, 240, 40, 80);
+        GameBoy bottom = new GameBoy(320, 300, 100, 180);
+        GameBoy left = new GameBoy(10, 240, 40, 80);
+
+        g2.setColor(Color.RED);
+        g2.draw(top);
+        g2.setColor(Color.GREEN);
+        g2.draw(right);
+        g2.setColor(Color.BLUE);
+        g2.draw(bottom);
+        g2.setColor(Color.MAGENTA);
+        g2.draw(left);
+
+        GameBoyWithDisplay gbd1 = new GameBoyWithDisplay(100, 250, 50, 75);
+        g2.setColor(Color.green);
+        g2.draw(gbd1);
+
+        // Make a black house that's half the size,
+        // and moved over 150 pixels in x direction
+        Shape gbd2 = ShapeTransforms.scaledCopyOfLL(gbd1, 0.5, 0.5);
+        gbd2 = ShapeTransforms.translatedCopyOf(gbd2, 150, 0);
+        gbd2 = ShapeTransforms.rotatedCopyOf(gbd2, Math.PI);
+        g2.setColor(Color.BLACK);
+        g2.draw(gbd2);
+
+        // Here's a house that's 4x as big (2x the original)
+        // and moved over 150 more pixels to right.
+        gbd2 = ShapeTransforms.scaledCopyOfLL(gbd2, 4, 4);
+        gbd2 = ShapeTransforms.translatedCopyOf(gbd2, 150, 0);
+
+        // We'll draw this with a thicker stroke
+        Stroke thick = new BasicStroke(4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
+
+        // for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
+        // #002FA7 is "International Klein Blue" according to Wikipedia
+        // In HTML we use #, but in Java (and C/C++) its 0x
+
+        Stroke orig = g2.getStroke();
+        g2.setStroke(thick);
+        g2.setColor(new Color(0x002FA7));
+        g2.draw(gbd2);
+
+        // Draw two houses with Windows
+
+        GameBoyWithDisplay gbd4 = new GameBoyWithDisplay(50, 350, 40, 75);
+        GameBoyWithDisplay gbd5 = new GameBoyWithDisplay(200, 350, 200, 100);
+
+        g2.draw(gbd4);
+        g2.setColor(new Color(0x8F00FF));
+
+        // Rotate the second house 45 degrees around its center.
+        Shape hw3 = ShapeTransforms.rotatedCopyOf(gbd5, Math.PI / 4.0);
+        hw3 = ShapeTransforms.translatedCopyOf(hw3, -50, -200);
+
+        g2.draw(hw3);
+
+        g2.setStroke(orig);
+        g2.setColor(Color.BLACK);
+        g2.drawString("A collection of GameBoys in the style of Monet by Joel Bagan", 20, 20);
+    }
+
+    /**
+     * Draw a different picture with a few houses and coffee cups
+     */
+    public static void drawPicture3(Graphics2D g2) {
+        // label the drawing
+        g2.drawString("And here we observe some GameBoys in their natural habitat by Joel Bagan", 20, 20);
+
+        GameBoyWithDisplay large = new GameBoyWithDisplay(200, 50, 225, 150);
+        GameBoy small = new GameBoy(120, 50, 40, 30);
+
+        g2.setColor(Color.RED);
+        g2.draw(large);
+        g2.setColor(Color.blue);
+        g2.draw(small);
+    }
+}
---
+++ src/edu/ucsb/cs56/drawings/joelbagan/advanced/GameBoy.java
@@ -0,0 +1,112 @@
+package edu.ucsb.cs56.drawings.joelbagan.advanced;
+
+import edu.ucsb.cs56.drawings.joelbagan.simple.Circle;
+import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
+import edu.ucsb.cs56.drawings.utilities.ShapeTransforms;
+
+import java.awt.*;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+
+/**
+ * A vector drawing of a GameBoy that implements
+ * the Shape interface, and so can be drawn, as well as
+ * rotated, scaled, etc.
+ *
+ * @author Joel Bagan
+ * @version for CS56, W16, UCSB
+ */
+public class GameBoy extends GeneralPathWrapper implements Shape {
+    /**
+     * Constructor
+     *
+     * @param x      x coord of lower left corner of gameboy
+     * @param y      y coord of lower left corner of gameboy
+     * @param height of gameboy (Ratio of height to width = 148:90)
+     */
+    public GameBoy(double x, double y, double width, double height) {
+
+        //Using constants to make a hard coded GameBoy to scale later
+        final double ORIG_ULX = 100;
+        final double ORIG_ULY = 100;
+        final double ORIG_HEIGHT = 150;
+        final double ORIG_WIDTH = 90;
+        GeneralPath image = new GeneralPath();
+        makeOutline(image);
+        makeScreen(image);
+        makeButtons(image);
+        makeDPad(image);
+        Shape s = ShapeTransforms.translatedCopyOf(image, -ORIG_ULX + x, -ORIG_ULY + y);
+        s = ShapeTransforms.scaledCopyOf(s, width / ORIG_WIDTH, height / ORIG_HEIGHT);
+        this.set(new GeneralPath(s));
+    }
+
+    /**
+     * Method to create the outline of the Gameboy
+     *
+     * @param image the GeneralPath that the outline is appended to
+     */
+    private void makeOutline(GeneralPath image) {
+        Shape outline = new Rectangle2D.Double(100, 100, 90, 150);
+        image.append(outline, false);
+    }
+
+    /**
+     * Method to create the screen of the Gameboy
+     *
+     * @param image the GeneralPath that the screen is appended to
+     */
+    private void makeScreen(GeneralPath image) {
+        Shape screen = new Rectangle2D.Double(122.5, 122.5, 45, 45);
+        image.append(screen, false);
+    }
+
+    /**
+     * Method to create the buttons on the GameBoy
+     * Utilizes the Circle class defined in .../joelbagan/simple
+     *
+     * @param image the GeneralPath that the buttons are appended to
+     */
+    private void makeButtons(GeneralPath image) {
+        Shape aButton = new Circle(160, 210, 5);
+        Shape bButton = new Circle(170, 200, 5);
+
+        Shape startButton = new RoundRectangle2D.Double(130, 240, 9, 3, 3, 3);
+        Shape selectButton = ShapeTransforms.translatedCopyOf(startButton, 20, 0);
+        image.append(startButton, false);
+        image.append(selectButton, false);
+        image.append(aButton, false);
+        image.append(bButton, false);
+    }
+
+    /**
+     * Method to create the DPad on the GameBoy
+     * Creates one quarter of the DPad and then copies, rotates, and translates
+     * to create the other 3 sides
+     *
+     * @param image the GeneralPath that the DPad is appended to
+     */
+    private void makeDPad(GeneralPath image) {
+        GeneralPath DPadLeft = new GeneralPath();
+        DPadLeft.moveTo(122.5, 200);
+        DPadLeft.lineTo(122.5 + 6, 200);
+        DPadLeft.moveTo(122.5, 200);
+        DPadLeft.lineTo(122.5, 200 + 6);
+        DPadLeft.lineTo(122.5 + 6, 200 + 6);
+
+        Shape DpadTop = ShapeTransforms.rotatedCopyOf(DPadLeft, Math.PI / 2);
+        DpadTop = ShapeTransforms.translatedCopyOf(DpadTop, 6, -6);
+
+        Shape DpadRight = ShapeTransforms.rotatedCopyOf(DpadTop, Math.PI / 2);
+        DpadRight = ShapeTransforms.translatedCopyOf(DpadRight, 6, 6);
+
+        Shape DPadBottom = ShapeTransforms.rotatedCopyOf(DpadRight, Math.PI / 2);
+        DPadBottom = ShapeTransforms.translatedCopyOf(DPadBottom, -6, 6);
+
+        image.append(DPadLeft, false);
+        image.append(DpadTop, false);
+        image.append(DpadRight, false);
+        image.append(DPadBottom, false);
+    }
+}
